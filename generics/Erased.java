38e774a0e445bd2a6043fc18be5b480462257323
wuxiongbo <wuxiongbo@mycs.cn>
2021-10-27 16:35:45 +0800

    泛型

--- generics/Erased.java
+++ generics/Erased.java
@@ -7,14 +7,17 @@
 /**
  * 创建泛型的实例
  *
- * 因为泛型擦除，我们将失去执行泛型代码中某些操作的能力。无法在运行时知道确切类型
+ * 因为泛型擦除，我们将失去执行泛型代码中某些操作的能力。
  *
- * 如代码所示，试图在 Erased.java 中 new T() 是行不通的，
+ * 1.无法在运行时知道确切类型
+ *
+ * 2.如代码所示，试图在 Erased.java 中 new T() 是行不通的，原因是：
  *      一是 由于泛型擦除，
  *      二是 编译器无法验证 T 是否具有默认（无参）构造函数。
- * 但是在 C++ 中，此操作 非常的 自然、直接 且 安全
- * 见： generics/InstantiateGenericType.cpp
+ *   但是，在 C++ 中，此操作 非常的 自然、直接 且 安全
+ *   见： generics/InstantiateGenericType.cpp
  *
+ * 3.无法创建泛型数组
  *
  * 一些情况下，我们可以对这些问题进行编程，
  * 但有些情况，我们必须通过 引入 “类型标签” 来补偿擦除。
@@ -34,14 +37,13 @@ public class Erased<T> {
     private final int SIZE = 100;
 
     public void f(Object arg) {
-//    if(arg instanceof T) {}
+//        if(arg instanceof T) {}
 
-//    T var = new T();
+//        T var = new T();
 
-//    T[] array = new T[SIZE];
+//        T[] array = new T[SIZE];
 
-        // warning: [unchecked] unchecked cast
-        T[] array = (T[]) new Object[SIZE];
+        T[] array = (T[]) new Object[SIZE]; // warning: [unchecked] unchecked cast
     }
 
 
